eeaa4daeb22dc18d76255b0006097aeebc14d960
sandfairy1219 <141256422+sandfairy1219@users.noreply.github.com>
2024-04-03 11:49:59 +0900

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+main(){
+int n, a; p.137 활동1
+scanf("%d%d", &n, &a);
+printf("%d+%d=%d\n", n, a, n+a);
+printf("%d-%d=%d\n", n, a, n-a);
+printf("%d*%d=%d\n", n, a, n*a);
+printf("%d/%d=%d\n", n, a, n/a);
+printf("%d% %%d=%d\n", n, a, n%a);
+int a, n; p.137 활동2
+scanf("%d%d", &a, &n);
+printf("넒이 = %d", a*n/2);
+int n ; p.137 활동3
+scanf("%d", &n);
+printf("넓이 = %1lf", n*n*3.14);
+}
