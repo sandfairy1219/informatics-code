1abeb5a4c46522d9a0fd9818b8278b09f4623816
sandfairy1219 <141256422+sandfairy1219@users.noreply.github.com>
2024-04-03 12:29:50 +0900

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+main()                                   
+{
+	
+	char n;
+	scanf("%c", &n);
+	if(n == 'A')
+	printf("good good");
+	else if(n == 'B')
+	printf("good");
+	else if(n == 'C')
+	printf("god");
+	else
+	printf("oh my god");
+	
+	 
+}
