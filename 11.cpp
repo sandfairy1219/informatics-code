b059c71b2b64e95b34f1e1277e9b32be9fcc05a4
sandfairy1219 <141256422+sandfairy1219@users.noreply.github.com>
2024-05-22 12:23:14 +0900

    Update 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 main()
 {
-//	int a;                                 // p.154 1 ¹ø 
-//	int i;
-//	scanf("%d", &a);
-//	for (i = 1;i<=a;i++)
-//	{
-//		
-//		printf("%d\n", i);
-//		
-//	}
-//	
-//	int a;                                 // p.154 2 ¹ø 
-//	int i;
-//	scanf("%d", &a);
-//	for (i = 1;i<=a;i++)
-//	{
-//		if(a%i == 0)
-//		printf("%d\n", i);
-//		
-//	}
+	int a;                                 // p.154 1번
+	int i;
+	scanf("%d", &a);
+	for (i = 1;i<=a;i++)
+	{
+		
+		printf("%d\n", i);
+		
+	}
 	
-	int a, i;
+	int a;                                 // p.154 2번
+	int i;
+	scanf("%d", &a);
+	for (i = 1;i<=a;i++)
+	{
+		if(a%i == 0)
+		printf("%d\n", i);
+		
+	}
+	
+	int a, i;                            //p.154 3번
 	scanf("%d", &a);
 	for (i = a;i>=1;i--)
 	{
